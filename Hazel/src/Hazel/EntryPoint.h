7a20c077eab4ec2eafc608956428f07dde067301
Sean Wang <seanwang313@outlook.com>
2022-02-14 13:07:46 +0800

    Added log system.

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,7 +5,13 @@
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv) {
-	printf("Hazel Engine\n");
+
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
